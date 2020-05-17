873e97b108e84fb5b416554f9a325365fbcefd60
Megan Comeau <mlcomeau99@gmail.com>
2020-05-17 23:39:14 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,8 +2,8 @@ class Dog
   attr_accessor :name, :breed, :id
 
   def initialize (id = nil, name:, breed:)
-    @name = name:
-    @breed = breed:
+    @name = name
+    @breed = breed
     @id = id
   end
 
