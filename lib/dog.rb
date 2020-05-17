3c8c0154cb30a825b36cd1972255db192b0d7f2e
Megan Comeau <mlcomeau99@gmail.com>
2020-05-17 23:36:42 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,22 +1,22 @@
-class Dog 
-  attr_accessor :name, :breed, :id 
+class Dog
+  attr_accessor :name, :breed, :id
 
   def initialize (name:, breed:, id=nil)
     @name = name
-    @breed = breed 
-    @id = id 
-  end 
+    @breed = breed
+    @id = id
+  end
 
-  def self.create_table 
-    sql = <<-SQL 
+  def self.create_table
+    sql = <<-SQL
     CREATE TABLE IF NOT EXISTS dogs (
-      id INTEGER PRIMARY KEY, 
+      id INTEGER PRIMARY KEY,
       name TEXT,
       breed TEXT
     )
     SQL
     DB[:conn].execute(sql)
-  end 
+  end
 
-  
-end 
+
+end
