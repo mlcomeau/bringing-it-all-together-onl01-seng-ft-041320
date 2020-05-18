f9258cb2a517fb11559f12283f7a831b64e325ca
Megan Comeau <mlcomeau99@gmail.com>
2020-05-18 00:11:13 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -44,11 +44,12 @@ class Dog
   end
 
   def self.new_from_db(row)
-    new_dog = self.new
-    new_dog.id = row[0]
-    new_dog.name = row[1]
-    new_dog.breed = row[2]
-    new_dog
+    attributes_hash = {
+      :id => row[0],
+      :name => row[1],
+      :breed => row[2]
+    }
+    self.new(attributes_hash)
   end
 
 
